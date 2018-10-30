57d967425c5f75467c51eee38c3011db822f3d4c
pierre <pierre.duval@gadz.org>
2018-10-30 21:55:00 +1100

    add base of html and setup webpack

---
+++ MineFieldConsoleRenderer.ts
@@ -0,0 +1,25 @@
+import { MineField } from "./MineField";
+import { TileType } from './Tile';
+
+export class MineFieldConsoleRenderer {
+    constructor() {}
+
+    render(mineField: MineField) {
+        for(let i = 0; i < mineField.tiles.length; i++) {
+            let lineString = '';
+    
+            for(let j = 0; j < mineField.tiles[0].length; j++) {
+                let tile = mineField.tiles[i][j];
+    
+                if (tile.type == TileType.Empty) {
+                    lineString += '_';
+                } else if (tile.type == TileType.Number) {
+                    lineString += `${tile.neighbouringMines}`;
+                } else if (tile.type == TileType.Mine) {
+                    lineString += 'X';
+                }
+            }
+            console.log(lineString);
+        }
+    }
+}
